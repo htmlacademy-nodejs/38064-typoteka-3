5682d817c25fc1cf6bcbbcd10c97638dff8ae600
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2020-11-21 22:26:41 +0300

    Добавляет логирование в файл

--- src/service/lib/logger.js
+++ src/service/lib/logger.js
@@ -1,9 +1,12 @@
 'use strict';
 
+const path = require(`path`);
 const pino = require(`pino`);
 const {Env} = require(`../../utils/const`);
 
 
+const LOG_PATH = `../logs/api.log`;
+const LOG_FILE = path.resolve(__dirname, LOG_PATH);
 const isDevMode = process.env.NODE_ENV === Env.DEVELOPMENT;
 const defaultLogLevel = isDevMode ? `info` : `error`;
 
@@ -11,7 +14,7 @@ const logger = pino({
   name: `base-logger`,
   level: process.env.LOG_LEVEL || defaultLogLevel,
   prettyPrint: isDevMode,
-});
+}, isDevMode ? process.stdout : pino.destination(LOG_FILE));
 
 const getLogger = (options = {}) => logger.child(options);
 
