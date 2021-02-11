b936691cc8b17d6081f8f7c86b35733d476a06ed
Nikolay Dumchev <nikolay.dumchev@gmail.com>
2021-02-11 17:19:52 +0300

    Добавляет запросы для заполнения таблиц и рефакторит запросы по созданию схемы

--- role.sql
+++ role.sql
@@ -1,3 +1,4 @@
+-- Пароль учетной записи для работы с БД typoteka: 1typoteka1
 CREATE ROLE typoteka WITH
     LOGIN
     NOSUPERUSER
@@ -5,4 +6,40 @@ CREATE ROLE typoteka WITH
     NOCREATEROLE
     INHERIT
     NOREPLICATION
-    CONNECTION LIMIT -1;
+    CONNECTION LIMIT -1
+    PASSWORD '1typoteka1';
+
+
+CREATE DATABASE typoteka
+    WITH
+    OWNER = typoteka
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    CONNECTION LIMIT = -1;
+GRANT ALL PRIVILEGES ON DATABASE typoteka TO typoteka;
+
+
+-- Перед созданием схемы подключится к базе typoteka
+CREATE SCHEMA main;
+GRANT ALL PRIVILEGES ON SCHEMA main to typoteka;
+
+ALTER DEFAULT PRIVILEGES
+    FOR USER typoteka
+    IN SCHEMA main
+    GRANT ALL PRIVILEGES ON TABLES TO typoteka;
+
+ALTER DEFAULT PRIVILEGES
+    FOR USER typoteka
+    IN SCHEMA main
+    GRANT ALL PRIVILEGES ON FUNCTIONS TO typoteka;
+
+ALTER DEFAULT PRIVILEGES
+    FOR USER typoteka
+    IN SCHEMA main
+    GRANT ALL PRIVILEGES ON SEQUENCES TO typoteka;
+
+ALTER DEFAULT PRIVILEGES
+    FOR USER typoteka
+    IN SCHEMA main
+    GRANT ALL PRIVILEGES ON ROUTINES TO typoteka;
